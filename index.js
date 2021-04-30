8c375efc3eb86a242601f02943ddabad42214948
Akash Ahire <akasha3001@gmail.com>
2021-04-30 13:54:30 +0530

    sqlite db added

--- index.js
+++ index.js
@@ -5,6 +5,21 @@
 import {AppRegistry} from 'react-native';
 import {RNAndroidNotificationListenerHeadlessJsName} from 'react-native-android-notification-listener';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import SQLite from 'react-native-sqlite-storage';
+
+global.db = SQLite.openDatabase(
+  {
+    name: 'SQLite',
+    location: 'default',
+    createFromLocation: '~SQLite.db',
+  },
+  () => {
+    console.log('db connected');
+  },
+  error => {
+    console.log('ERROR: ' + error);
+  },
+);
 
 import App from './App';
 import {name as appName} from './app.json';
